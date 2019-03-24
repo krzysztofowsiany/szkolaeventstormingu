4f23391cd6975099d92e6fd859c853c926256a8b
Krzysztof Owsiany <gocom@o2.pl>
2019-03-24 11:40:36 +0100

    Add initial landing page to repository

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const rename = require('gulp-rename');
+
+gulp.task('sass', () => {
+
+    return gulp.src('./build/sass/custom.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'expanded'}))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./assets/css'));
+});
+
+gulp.task('sassMin', () => {
+
+    return gulp.src('./build/sass/custom.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(rename('custom.min.css'))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./assets/css'));
+});
+
+
+gulp.task('watch', () => {
+
+    gulp.watch('./build/sass/**/*.scss', gulp.parallel('sass', 'sassMin'));
+});
