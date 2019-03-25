27985c742152906244853e2969ecf1b66f3db4ce
Krzysztof Owsiany <gocom@o2.pl>
2019-03-25 17:57:21 +0100

    Add privacy policy

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ const sourcemaps = require('gulp-sourcemaps');
 const rename = require('gulp-rename');
 
 gulp.task('sass', () => {
-
     return gulp.src('./build/sass/custom.scss')
         .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'expanded'}))
@@ -18,7 +17,6 @@ gulp.task('sass', () => {
 });
 
 gulp.task('sassMin', () => {
-
     return gulp.src('./build/sass/custom.scss')
         .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'compressed'}))
@@ -31,8 +29,36 @@ gulp.task('sassMin', () => {
         .pipe(gulp.dest('./assets/css'));
 });
 
+gulp.task('private-policy-sass', () => {
+    return gulp.src('./build/sass/private-policy.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'expanded'}))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./assets/css'));
+});
+
+
+
+
+
+gulp.task('private-policy-sassMin', () => {
+    return gulp.src('./build/sass/private-policy.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(rename('private-policy.min.css'))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./assets/css'));
+});
+
 
 gulp.task('watch', () => {
-
-    gulp.watch('./build/sass/**/*.scss', gulp.parallel('sass', 'sassMin'));
+    gulp.watch('./build/sass/**/*.scss', gulp.parallel('sass', 'sassMin','private-policy-sass', 'private-policy-sassMin'));
 });
